9d27f2843e77a559be940388af8275da166bea74
zhenzi0322 <82131529@qq.com>
2026-07-19 00:04:19 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 package = {
     "name": "color-convert",
-    "version": "1.1",
+    "version": "1.2",
     "description": "Color Conversion",
     "keywords": [
         "color",
